9940afdbf1941bade38d4730f443141bbbc5117a
ihashing <47662791+ihashing@users.noreply.github.com>
2021-08-26 18:14:53 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,12 @@
-self.addEventListener("fetch", function(event){
-
+self.addEventListener('fetch', function(event){
+   event.respondWith(async function () {
+      var cache = await caches.open('cache');
+      var cachedResponsePromise = await cache.match(event.request);
+      var networkResponsePromise = fetch(event.request);
+      event.waitUntil(async function () {
+         var networkResponse = await networkResponsePromise;
+         await cache.put(event.request, networkResponse.clone());
+      }());
+      return cachedResponsePromise || networkResponsePromise;
+    }());
 });
